ac278ed9ff4365bb5c5913fcd8423d83371ded6e
Graham Fairweather <xotic750@gmail.com>
2019-07-17 20:16:24 +0200

    :bookmark: v2.0.14
    Auto build

--- dist/same-value-zero-x.js
+++ dist/same-value-zero-x.js
@@ -2,13 +2,13 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2015-present",
-  "date": "2019-07-17T17:16:06.390Z",
+  "date": "2019-07-17T18:16:19.426Z",
   "describe": "",
   "description": "ES6-compliant shim for SameValueZero.",
   "file": "same-value-zero-x.js",
   "hash": "8b515394f18241f8bca0",
   "license": "MIT",
-  "version": "2.0.13"
+  "version": "2.0.14"
 }
 */
 (function webpackUniversalModuleDefinition(root, factory) {
