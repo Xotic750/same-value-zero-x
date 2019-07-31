792efde6cb5c78899ce32a27125d49f4c1c69296
Graham Fairweather <xotic750@gmail.com>
2019-07-31 17:33:21 +0200

    Package rework

--- dist/same-value-zero-x.js
+++ dist/same-value-zero-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2015-present",
-  "date": "2019-07-29T21:37:10.046Z",
+  "date": "2019-07-31T15:33:15.023Z",
   "describe": "",
   "description": "ES6-compliant shim for SameValueZero.",
   "file": "same-value-zero-x.js",
